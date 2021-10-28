a32d769ebcafd7ea4534d4f11d7a7f46501ce719
JayKuriN <69405557+JayKuriN@users.noreply.github.com>
2021-10-28 17:58:19 -0500

    Dudde
    i farted

--- _anim/Imp.asm
+++ _anim/Imp.asm
@@ -6,7 +6,7 @@ Ani_Imp:	dc.w @walk1-Ani_Imp
 		dc.w @digging-Ani_Imp
 		dc.w @fall-Ani_Imp
 @walk1:		dc.b 8,	0, afEnd
-@walk2:		dc.b 15, 1, 0, 2, 0, afEnd
+@walk2:		dc.b 3, 1, 1, 1, 1, 0, 2, 2, 2, 2, 0, afEnd
 @digging:	dc.b 8, 3, afEnd
 @fall:		dc.b 8, 3, afEnd
 @dead:		dc.b 4,	4, 5, afEnd
