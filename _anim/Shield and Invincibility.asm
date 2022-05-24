006d19669c7007bc999b2d8e604b374022ba0196
MCTravisYT <59547361+MCTravisYT@users.noreply.github.com>
2022-05-24 16:33:04 -0400

    finished invincible

--- _anim/Shield and Invincibility.asm
+++ _anim/Shield and Invincibility.asm
@@ -1,14 +1,13 @@
 ; ---------------------------------------------------------------------------
 ; Animation script - shield and invincibility stars
 ; ---------------------------------------------------------------------------
-Ani_Shield:	dc.w @shield-Ani_Shield
+Ani_Shield:	dc.w @stars1-Ani_Shield
 		dc.w @stars1-Ani_Shield
 		dc.w @stars2-Ani_Shield
 		dc.w @stars3-Ani_Shield
 		dc.w @stars4-Ani_Shield
-@shield:	dc.b 5,	1, 2, 3, 4, 5, 6, afEnd
-@stars1:	dc.b 5,	1, 2, 3, afEnd
-@stars2:	dc.b 5,	1, 2, 3, afEnd
-@stars3:	dc.b 5,	1, 2, 3, afEnd
-@stars4:	dc.b 5,	1, 2, 3, afEnd
+@stars1:	dc.b 2,	1, 0, 6, 0, 3, 0, 8, 0, 5, 0, 2, 0, 7, 0, 4, 0, afEnd
+@stars2:	dc.b 2,	0, 6, 0, 3, 0, 8, 0, 5, 0, 2, 0, 7, 0, 4, 0, 1, afEnd
+@stars3:	dc.b 2,	6, 0, 3, 0, 8, 0, 5, 0, 2, 0, 7, 0, 4, 0, 1, 0, afEnd
+@stars4:	dc.b 2,	0, 3, 0, 8, 0, 5, 0, 2, 0, 7, 0, 4, 0, 1, 0, 6, afEnd
 		even
