f596bd21289b26dda63a6fc656da2eacf1f04000
MCTravisYT <59547361+MDTravisYT@users.noreply.github.com>
2023-04-24 22:39:33 -0400

    restored csz and imz

--- _inc/PaletteCycle.asm
+++ _inc/PaletteCycle.asm
@@ -24,15 +24,16 @@ PCycle_Index:	dc.w PCycle_GHZ-PCycle_Index
 		dc.w PalCycle_SLZ-PCycle_Index
 		dc.w PalCycle_SYZ-PCycle_Index
 		dc.w PalCycle_SBZ-PCycle_Index
-		dc.w PCycle_MZ-PCycle_Index	; Ending
-		dc.w PCycle_MZ-PCycle_Index
-		dc.w PCycle_MZ-PCycle_Index
-		dc.w PCycle_MZ-PCycle_Index
-		dc.w PCycle_MZ-PCycle_Index
+		dc.w PCycle_Null-PCycle_Index	; Ending
+		dc.w PCycle_Null-PCycle_Index
+		dc.w PCycle_Null-PCycle_Index
+		dc.w PCycle_Null-PCycle_Index
+		dc.w PCycle_Null-PCycle_Index
 
 
 ; ||||||||||||||| S U B	R O U T	I N E |||||||||||||||||||||||||||||||||||||||
-
+PCycle_Null:
+		rts
 
 PCycle_Title:
 		lea	(Pal_TitleCyc).l,a0
