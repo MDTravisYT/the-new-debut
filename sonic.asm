eb896a77cbf65e0352ec20f17c3432484edb4df9
KGLBombyPig <kgl1@comcast.net>
2023-05-15 22:36:08 -0400

    s

--- sonic.asm
+++ sonic.asm
@@ -1992,7 +1992,7 @@ RadNexAff_PalLoop:
     move.l  (a0)+,(a1)+         ; ''
     dbf d0,RadNexAff_PalLoop      ; repeat until done
     jsr PaletteFadeIn          ; fade palette in
-    move.w  #300,($FFFFF614).w     ; set delay time (3 seconds on a 60hz system)
+    move.w  #800,($FFFFF614).w     ; set delay time (3 seconds on a 60hz system)
  
 Rad_MainLoop:
     move.b  #2,($FFFFF62A).w        ; set V-blank routine to run
