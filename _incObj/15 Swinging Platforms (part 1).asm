76f8fef2b4fc713420d69764e537c4203714ee4f
Geeovanni <iamgiovanni.gb@gmail.com>
2023-05-17 23:52:15 +0200

    ball no hurtie :)

--- _incObj/15 Swinging Platforms (part 1).asm
+++ _incObj/15 Swinging Platforms (part 1).asm
@@ -109,7 +109,8 @@ Swing_Main:	; Routine 0
 		move.w	#$43AA,obGfx(a0)
 		move.b	#1,obFrame(a0)
 		move.b	#2,obPriority(a0)
-		move.b	#$81,obColType(a0) ; make object hurt when touched
+		move.b	#$10,obHeight(a0)	; GIO: different hitbox		
+;		move.b	#$81,obColType(a0)	; make object NOT hurt when touched
 
 	@not1X:
 		cmpi.b	#id_CWZ,(v_zone).w ; is zone SBZ?
